11262a7ef9c8efcb40745c94d699fd4835dac339
Michael <mgeleeming@gmail.com>
2022-08-05 14:40:15 +1000

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name = "synthedia",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "Generate synthetic DIA LC-MS/MS proteomics data",
     long_description = README,
     long_description_content_type = "text/markdown",
